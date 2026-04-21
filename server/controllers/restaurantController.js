e2c633115232afb57f21e43d4da090cca0ea6d26
Alkendi-1 <a.alkendi26@gmail.com>
2026-04-21 21:47:12 +0200

    feat: add restaurant controller with CRUD logic

---
+++ server/controllers/restaurantController.js
@@ -0,0 +1,64 @@
+const Restaurant = require('../models/restaurantModel');
+
+exports.createRestaurant = async (req, res) => {
+  try {
+    const newRestaurant = await Restaurant.create(req.body);
+    res.status(201).json(newRestaurant);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getRestaurants = async (req, res) => {
+  try {
+    const { city, name } = req.query;
+
+    let query = {};
+
+    if (city) {
+      query.city = city;
+    }
+
+    if (name) {
+      query.name = { $regex: name, $options: 'i' };
+    }
+
+    const restaurants = await Restaurant.find(query);
+
+    res.json(restaurants);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.updateRestaurant = async (req, res) => {
+  try {
+    const updatedRestaurant = await Restaurant.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedRestaurant) {
+      return res.status(404).json({ message: 'Restaurant not found' });
+    }
+
+    res.json(updatedRestaurant);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteRestaurant = async (req, res) => {
+  try {
+    const deletedRestaurant = await Restaurant.findByIdAndDelete(req.params.id);
+
+    if (!deletedRestaurant) {
+      return res.status(404).json({ message: 'Restaurant not found' });
+    }
+
+    res.json({ message: 'Deleted successfully' });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
