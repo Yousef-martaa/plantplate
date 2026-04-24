e015de843207f0e36ebd166951444c757f525717
Alkendi-1 <a.alkendi26@gmail.com>
2026-04-24 10:57:14 +0200

    feat: add user model and basic user route

--- server/index.js
+++ server/index.js
@@ -3,17 +3,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const app = express(); // ✅ لازم يكون أول شي
-
+const app = express();
 app.use(express.json());
 app.use(cors());
 
 // routes
 const restaurantRoutes = require('./routes/restaurantRoutes');
 const reviewRoutes = require('./routes/reviewRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 app.use('/api', restaurantRoutes);
 app.use('/api', reviewRoutes);
+app.use('/api', userRoutes);
 
 // DB
 mongoose.connect(process.env.MONGO_URI)
